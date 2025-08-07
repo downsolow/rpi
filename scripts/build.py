d44f36df253b4d9fad9311510d167e03b338538e
Tumidajewicz <jakub.tumidajewicz@capgemini.com>
2025-08-07 14:43:14 +0200

    Add target installation if missing

--- scripts/build.py
+++ scripts/build.py
@@ -1,4 +1,15 @@
 import subprocess
 
+TARGET = "aarch64-unknown-linux-gnu"
+
+def target_missing():
+    output = subprocess.run("rustup target list --installed", capture_output=True, shell=True).stdout
+    return output.decode("utf-8").find(TARGET) == -1
+
+def install_target():
+    subprocess.run(f"rustup target add {TARGET}", shell=True)
+
 def build(project_root: str):
-    subprocess.run("cargo build --target aarch64-unknown-linux-gnu --release", cwd = project_root, check = True)
+    if target_missing():
+        install_target()
+    subprocess.run(f"cargo build --target {TARGET} --release", cwd = project_root, check = True, shell=True)
