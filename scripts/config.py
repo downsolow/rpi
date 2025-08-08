fad79b64b1328938b7a744380ac89bab3df6693a
downsolow <ddszulkowski@gmail.com>
2025-08-08 22:00:23 +0200

    Add copying ssh keys from Linux to RPi

--- scripts/config.py
+++ scripts/config.py
@@ -1,19 +1,20 @@
 import os
 import subprocess
 
-def  check_ssh_connection(host, timeout=5):
+def  check_host_available(host, timeout=5):
     try:
         result = subprocess.run(
             ["ssh", "-o", f"ConnectTimeout={timeout}", "-o", "BatchMode=yes", host, "exit"],
             stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE
+            stderr=subprocess.PIPE,
+            shell = True
         )
-        if result.returncode == 0:
-            return True
-        else:
-            return False
-    except Exception as e:
+        
+        return True
+    except subprocess.TimeoutExpired:
         return False
+    except Exception as e:
+        return True
 
 
 
@@ -22,5 +23,5 @@ binary_name = "app"
 binary_path = os.path.join(project_root, "target", "aarch64-unknown-linux-gnu", "release", binary_name)
 rpi_user = "erd"
 rpi_dns_host = "raspberrypi1.local"
-rpi_host = rpi_dns_host if check_ssh_connection(rpi_dns_host) else "192.168.1.100"
+rpi_host = rpi_dns_host if check_host_available(rpi_dns_host) else "192.168.1.100"
 destination = "/home/erd/training/hello_world_diode"
