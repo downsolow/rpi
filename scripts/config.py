7565a135acacd680e2b37652826d7b846b954b0b
downsolow <ddszulkowski@gmail.com>
2025-08-08 22:00:23 +0200

    Add copying ssh keys from Linux to RPi

--- scripts/config.py
+++ scripts/config.py
@@ -1,19 +1,20 @@
 import os
 import subprocess
 
-def  check_ssh_connection(host, timeout=5):
+def  check_host_available(host, timeout=5):
     try:
         result = subprocess.run(
             ["ssh", "-o", f"ConnectTimeout={timeout}", "-o", "BatchMode=yes", host, "exit"],
             stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE
+            stderr=subprocess.PIPE,
+            shell = True
         )
-        if result.returncode == 0:
-            return True
-        else:
-            return False
-    except Exception as e:
+        
+        return True
+    except subprocess.TimeoutExpired:
         return False
+    except Exception as e:
+        return True
 
 
 
@@ -21,6 +22,6 @@ project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 binary_name = "app"
 binary_path = os.path.join(project_root, "target", "aarch64-unknown-linux-gnu", "release", binary_name)
 rpi_user = "erd"
-rpi_dns_host = "raspberrypi1.local"
-rpi_host = rpi_dns_host if check_ssh_connection(rpi_dns_host) else "192.168.1.100"
+rpi_dns_host = "raspberrypi.local"
+rpi_host = rpi_dns_host if check_host_available(rpi_dns_host) else "192.168.1.100"
 destination = "/home/erd/training/hello_world_diode"
--- scripts/copy_ssh_key.py
+++ scripts/copy_ssh_key.py
@@ -1,21 +1,37 @@
+#!/usr/bin/env python3
+
 import subprocess
 import os
 from config import *
+import platform
 
-rpi_user = f"{rpi_user}@{rpi_host}"
-check_ssh_already_copied_command = f"ssh -o BatchMode=yes {rpi_user} true"
-ssh_check_result = subprocess.run(check_ssh_already_copied_command)
 
-if ssh_check_result.returncode == 0:
-    print("ssh already copied")
-else:
-    user_profile = subprocess.run(["powershell", "-Command", "echo $env:USERPROFILE"], capture_output = True, text = True).stdout.strip()
+def copy_ssh_key():
+    user = f"{rpi_user}@{rpi_host}"
+    check_ssh_already_copied_command = f"ssh -o BatchMode=yes {user} true"
+    ssh_check_result = subprocess.run(check_ssh_already_copied_command, shell = True)
+
+    if ssh_check_result.returncode == 0:
+        print("ssh already copied")
+        return
+
+    user_profile = (subprocess.run(
+        ["powershell", "-Command", "echo $env:USERPROFILE"], 
+        shell = True, 
+        capture_output = True, 
+        text = True
+    ).stdout.strip()) if platform.system == "Windows" else os.path.expanduser("~")
     ssh_public_key_path = os.path.join(user_profile, ".ssh", "id_ed25519.pub")
 
     if not os.path.exists(ssh_public_key_path):
+        print(f"ssh key path: {ssh_public_key_path} not found")
         print("Generating new ssh key..")
         subprocess.run("ssh-keygen")
 
     print("Copying ssh key to RPi..")
-    ssh_copy_command =  f'type {ssh_public_key_path} | ssh {rpi_user} "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys"'
-    subprocess.run(["powershell", "-Command", ssh_copy_command])
+    ssh_copy_command =  f'ssh {user} "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys" < {ssh_public_key_path}'
+    subprocess.run(ssh_copy_command, shell=True)
+
+
+if __name__ == "__main__":
+    copy_ssh_key()
