2e5f10fbcb043ff9eadf218b9f8eee30935137a2
downsolow <ddszulkowski@gmail.com>
2025-08-12 15:55:12 +0200

    Add remote build on binary destination host

--- scripts/config.py
+++ scripts/config.py
@@ -1,27 +1,62 @@
+#!/usr/bin/env python3
 import os
 import subprocess
+import socket
 
-def  check_host_available(host, timeout=5):
+
+def get_project_root():
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+
+def get_binary_name():
+    cargo_toml = f"{project_root}/Cargo.toml"
+    with open(cargo_toml) as file:
+        for line in file.readlines():
+            if line.startswith("name = "):
+                return line.split(" = ")[-1]
+
+
+def is_host_available(host):
     try:
-        result = subprocess.run(
-            ["ssh", "-o", f"ConnectTimeout={timeout}", "-o", "BatchMode=yes", host, "exit"],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            shell = True
-        )
-        
+        socket.gethostbyname(host)
         return True
-    except subprocess.TimeoutExpired:
+
+    except Exception:
         return False
-    except Exception as e:
-        return True
 
 
+def is_ssh_connectable(user: str, host: str, timeout=5):
+    user_host = f"{user}@{host}"
+    if is_host_available(host):
+        try:
+            result = subprocess.run(
+                f"ssh -o connectTimeout={timeout} {user_host} exit", 
+                stdout=subprocess.PIPE, 
+                stderr=subprocess.PIPE, 
+                timeout=timeout, 
+                shell=True
+            )
+            return result.returncode == 0
+        
+        except Exception:
+            return False
 
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-binary_name = "app"
+    return False
+
+
+def get_host(host: str):
+    if is_host_available(host):
+        return host
+    else:
+        default_static_ip = "192.168.1.100"
+        print(f"host {host} not available, using static ip: {default_static_ip}")
+        return default_static_ip
+
+
+project_root = get_project_root()
+binary_name = get_binary_name()
 binary_path = os.path.join(project_root, "target", "aarch64-unknown-linux-gnu", "release", binary_name)
 rpi_user = "erd"
-rpi_dns_host = "raspberrypi1.local"
-rpi_host = rpi_dns_host if check_host_available(rpi_dns_host) else "192.168.1.100"
+rpi_dns_host = "raspberrypi.local"
+rpi_host = get_host(rpi_dns_host)
 destination = "/home/erd/training/hello_world_diode"
